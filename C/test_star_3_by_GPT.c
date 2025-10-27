cce00978c506b92f71270b65a9a15128b4fc8bd6
MH_MAC <mh_mac@MACs-MacBook-Pro.local>
2025-10-27 08:01:56 +0500

    This is for practice of for loop to print stars

---
+++ C/test_star_3_by_GPT.c
@@ -0,0 +1,26 @@
+// output :
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+#include <stdio.h>
+int main()
+{
+     for (int i = 1; i <= 5; i = i + 1)
+     {
+          for (int k = i; k <= 5; k++)
+          {
+               printf(" ");
+          }
+          for (int j = 1; j <= (i*2-1); j++)
+          {
+               printf("*");
+          }
+          printf("\n");
+     }
+
+     return 0;
+}
